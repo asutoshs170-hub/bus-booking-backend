6a770d9400eaffbab6a0359d3ce238f19c03f618
ashutosh <asutoshs170@gmail.com>
2026-07-23 13:35:35 +0530

    Allow Netlify frontend in CORS

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://fastidious-chimera-13c562.netlify.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
